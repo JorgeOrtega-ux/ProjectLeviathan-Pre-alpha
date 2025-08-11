32c470b9e78dbf7e2840414489bfc991572fdfa1
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-11 11:04:12 -0500

    Delete ProjectLeviathan directory

--- ProjectLeviathan/includes/sections/accessibility-section.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content <?php echo $CURRENT_SUBSECTION === 'accessibility' ? 'active' : 'disabled'; ?>" data-section="sectionAccessibility">
-    <h2>Accesibilidad</h2>
-    <p>Configura las opciones de accesibilidad</p>
-</div>
--- ProjectLeviathan/includes/sections/profile-section.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content <?php echo $CURRENT_SUBSECTION === 'profile' ? 'active' : 'disabled'; ?>" data-section="sectionProfile">
-    <h2>Tu Perfil</h2>
-    <p>Administra la configuración de tu perfil de usuario</p>
-</div>
