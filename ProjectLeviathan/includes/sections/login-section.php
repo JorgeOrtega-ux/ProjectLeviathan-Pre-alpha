4f6058048cac7fbf23d33bac57bd41b73de6ed33
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-11 10:13:41 -0500

    Delete ProjectLeviathan directory

--- ProjectLeviathan/includes/sections/login-section.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content <?php echo $CURRENT_SUBSECTION === 'login' ? 'active' : 'disabled'; ?>" data-section="sectionLogin">
-    <h2>Iniciar Sesión</h2>
-    <p>Inicia sesión en tu cuenta</p>
-</div>
